2b81284b0bb1b6efc85a3d4a9fbe8202172b54f5
Ubuntu <ubuntu@ip-172-31-32-246.us-east-2.compute.internal>
2017-12-10 06:04:37 +0000

    Use buttom instead of ::after selector

--- js/index.js
+++ js/index.js
@@ -67,6 +67,10 @@ $(function(){
                 });
             }
         });
+        
+         $('.place-info-visibility-toggle').on('click', function() {
+             $('#place-info-wrapper').toggleClass('visible');
+         }); 
            
     };
 
@@ -79,7 +83,7 @@ $(function(){
             $('.place-name').text(place['name']);
             $('.place-review-score').text(place['rating']);
             $('.place-type').text(place['types'][0]);
-            $('#place-info-wrapper').show();
+            $('#place-info-wrapper').addClass('visible');
         });
     };
      
