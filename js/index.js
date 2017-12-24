61ea33ce6043d0926ac056467398b2d65f092d8e
Ubuntu <ubuntu@ip-172-31-32-246.us-east-2.compute.internal>
2017-12-24 02:01:04 +0000

    Slide in/out animation

--- js/index.js
+++ js/index.js
@@ -61,7 +61,10 @@ $(function(){
             placeId: place['place_id']
         };
         service.getDetails(params, function(place) {
-            $('#hero-header-wrapper img').attr('src', place.photos[0].getUrl({'maxWidth': 408, 'maxheight': 407}));
+            $('#hero-header-wrapper img').removeAttr('src');
+             if (place.photos){
+                 $('#hero-header-wrapper img').attr('src', place.photos[0].getUrl({'maxWidth': 408, 'maxheight': 407}));
+            }
             $('.place-name').text(place['name']);
             $('.place-review-score').text(place['rating']);
             $('.place-type').text(place['types'][0]);
